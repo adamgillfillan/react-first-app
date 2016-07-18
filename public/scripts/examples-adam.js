2f9dd34bbee9fffc2505be20630e9ad916b2c10d
Adam Gillfillan <agillfi@ncsu.edu>
2016-07-18 18:09:18 -0400

    bubble up the author/text data from the child commentForm to the parent commentBox and post the comment data to an endpoint.

--- public/scripts/examples-adam.js
+++ public/scripts/examples-adam.js
@@ -23,9 +23,7 @@ let CommentList = React.createClass({
   render: function() {
     var commentNodes = this.props.data.map(function(comment) {
       return (
-        <Comment author={comment.author} key={comment.id} text2={comment.text}>
-          {/*{comment.text}*/}
-        </Comment>
+        <Comment author={comment.author} key={comment.id} text={comment.text} />
       );
     });
     return (
@@ -37,21 +35,54 @@ let CommentList = React.createClass({
 });
 
 let CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+
+  handleAuthorChange: function(e) {
+    console.info('author event', e);
+    this.setState({author: e.target.value});
+  },
+
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    let author = this.state.author.trim();
+    let text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    }
+
+    this.props.onCommentSubmit({author, text});
+    this.setState({author: '', text: ''});
+  },
+
   render: function() {
     return (
-      <div className="commentForm">
-        Hello world! I am da CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something.."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+           />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
 
 let CommentBox = React.createClass({
-  getInitialState: function() {
-    return {data: []};
-  },
-
-  componentDidMount: function() {
+  loadCommentsFromServer: function() {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -61,8 +92,32 @@ let CommentBox = React.createClass({
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
-      }
+      }.bind(this)
     });
+  },
+
+  handleCommentSubmit: function(comment) {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   },
 
   render: function() {
@@ -70,10 +125,13 @@ let CommentBox = React.createClass({
       <div className="commentBox">
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
 });
 
-ReactDOM.render(<CommentBox url="/api/comments"/>, document.getElementById('content'));
+ReactDOM.render(
+  <CommentBox url="/api/comments" pollInterval={2000} />,
+  document.getElementById('content')
+);
