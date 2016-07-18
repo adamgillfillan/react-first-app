14c3f38c5f86455539faef5334de6516c0252472
Adam Gillfillan <agillfi@ncsu.edu>
2016-07-18 17:17:04 -0400

    use ajax to load in comment data

--- public/scripts/examples-adam.js
+++ public/scripts/examples-adam.js
@@ -47,15 +47,33 @@ let CommentForm = React.createClass({
 });
 
 let CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }
+    });
+  },
+
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
   }
 });
 
-ReactDOM.render(<CommentBox data={data}/>, document.getElementById('content'));
+ReactDOM.render(<CommentBox url="/api/comments"/>, document.getElementById('content'));
